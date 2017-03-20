4643ea9af797d6ba711090bd07bc6b6e04feb358
Thomas Geib <tgeib7353@vbtc.net>
2017-03-20 13:30:33 -0400

    Inital Commit

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	
+	cout << "Hello World" << endl;
+	
+	system("pause");
+	return 0;
+}
+
