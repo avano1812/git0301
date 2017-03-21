6652a3c2349e96b9deda20bd88c66d37bff9dd20
Th3 Doctor Phil <pvlietstra7966@vbtc.net>
2017-03-21 12:22:54 -0400

    Added play() function

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -14,3 +14,24 @@ int main() {
 	return 0;
 }
 
+void play(const string& aWord)
+{
+	string guess;
+	do{
+		cout << "Enter your guess: ";
+		cin >> guess;
+
+		if ((guess != aWord) && (guess != "quit"))
+		{
+			cout << "You are WERY WERY WRONG!!!!\n";
+		}
+	} while ((guess != aWord) && (guess != "quit"));
+
+	if (guess == aWord)
+	{
+		cout << "You got it!\n";
+	}
+
+	cout << "Thanks a lot for playing.\n";
+
+}
