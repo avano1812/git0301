5b2b84c863ff80b9f07ed95144656bfb79a33cfa
Thomas Geib <tgeib7353@vbtc.net>
2017-03-24 12:41:40 -0400

    Commit

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -14,3 +14,24 @@ int main() {
 	return 0;
 }
 
+
+//return a random word
+string randomWord()
+{
+	const int NUM_WORDS = 5;
+	const string WORDS[NUM_WORDS] =
+	{
+		"wall",
+		"glasses",
+		"labored",
+		"persistent",
+		"jumble"
+	};
+
+	int choice = (rand() % NUM_WORDS);
+	string word = WORDS[choice];
+
+	return word;
+
+
+}
